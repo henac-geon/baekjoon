53d4d6d08fc5ed6809d05098df95db63745751dd
man25816 <man25816@naver.com>
2023-10-30 19:51:59 +0900

    깃허브 수정

---
+++ baekjoon/중앙이동.cpp
@@ -0,0 +1,20 @@
+/*
+#include<iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void) {
+
+	int n, i;
+
+	cin >> n;
+
+	i = pow(2, n) + 1;
+	i = pow(i, 2);
+
+	cout << i;
+
+	return 0;
+}
+*/

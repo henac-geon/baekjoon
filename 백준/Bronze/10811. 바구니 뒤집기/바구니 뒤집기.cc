fc11fbdd9f753224fe2e682e8f7ad79a260992e7
geon-jegal <73727480+geon-jegal@users.noreply.github.com>
2023-11-14 20:17:06 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+    int* n;
+    int size, count, begin, end;
+    cin >> size >> count;
+    n = new int[size];
+    for (int i = 0; i < size; i++) n[i] = i + 1;
+
+    for (int i = 0; i < count; i++) {
+        cin >> begin >> end;
+        reverse(n + begin - 1, n + end);
+    }
+
+    for (int i = 0; i < size; i++) cout << n[i] << " ";
+
+    delete[] n;
+
+    return 0;
+}

7272959c0d2562075c00153ab2edd9aad99a9383
man25816@stu.kmu.ac.kr <man25816@stu.kmu.ac.kr>
2023-12-31 10:50:15 +0900

    실행 환경 수정

---
+++ baekjoon/test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(void) {
+	
+	std::cout << "hello world";
+	// int n, result;
+	//cin >> n >> result;
+
+	//int* arry = new int[n];
+
+	//for (int i = 0; i < n; i++) cin >> arry[i];
+
+
+	return 0;
+}

325c1e170da5202c494622637621786ca98fb99c
geon-jegal <73727480+geon-jegal@users.noreply.github.com>
2024-04-25 15:31:59 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 672 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int arr[1000000] = { };
+
+    cin >> a;
+    for (int i = 0; i < a; i++) cin >> arr[i];
+    
+    sort(arr, arr + a);
+
+    for (int i = 0; i < a; i++) cout << arr[i] << ' ';
+    return 0;
+}

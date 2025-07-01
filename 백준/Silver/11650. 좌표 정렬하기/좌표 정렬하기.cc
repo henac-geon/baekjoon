0dcaee5747cc59293f39ebe3833d3d1ca1aa1065
henac <73727480+henac-geon@users.noreply.github.com>
2025-07-01 21:51:40 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 108 ms, Memory: 3568 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+struct pos
+{
+	int x, y;
+};
+
+bool operator<(const pos& a, const pos& b) {
+	if (a.x == b.x) {
+		return a.y > b.y;
+	}
+	return a.x > b.x;
+}
+
+int main(void) {
+	int n, x, y;
+	cin >> n;
+
+	priority_queue<pos> arr;
+
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		pos p;
+		p.x = x;
+		p.y = y;
+		arr.push(p);
+	}
+
+	while (!arr.empty()) {
+		pos p = arr.top();
+		arr.pop();
+		cout << p.x << " " << p.y << "\n";
+	}
+
+	return 0;
+}
